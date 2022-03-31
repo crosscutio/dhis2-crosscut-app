515f2e18b546f2e0da8ac94595dced2809297947
luongt13 <65784084+luongt13@users.noreply.github.com>
2022-03-31 14:13:01 -0400

    clean

--- src/pages/ListCatchmentJobs.jsx
+++ src/pages/ListCatchmentJobs.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ky from "ky";
-import ButtonItem from "../components/ButtonItem/ButtonItem";
 import {
   Card,
   TableHead,
@@ -13,8 +11,9 @@ import styles from './ListCatchmentJobs.module.css'
 import JobDetails from "../components/JobDetails/JobDetails";
 import { fetchCatchmentJobs } from "../api/crosscutRequests";
 import { useToggle } from "../hooks/useToggle"
+import i18n from "../locales/index"
 
-function ListCatchmentJobs(props) {
+function ListCatchmentJobs() {
   const [jobs, setJobs] = useState(null)
   const [isToggled, toggle] = useToggle(false)
   const [{ column, direction }, setSortInstructions] = useState({
@@ -31,9 +30,10 @@ function ListCatchmentJobs(props) {
     setJobs(resp)
   }
 
-
-  const getSortDirection = (columnName) =>
-      columnName === column ? direction : 'default'
+  // handle sorting of columns
+  const getSortDirection = (columnName) => {
+    return columnName === column ? direction : 'default'
+  }
 
   const onSortIconClick = ({ name, direction }) => {
       setSortInstructions({
@@ -44,30 +44,30 @@ function ListCatchmentJobs(props) {
 
   return (
     <div className={styles.container}>
-    <Card>
-      <DataTable>
-        <TableHead>
-          <DataTableRow>
-            <DataTableColumnHeader fixed top="0" width="48px" onSortIconClick={onSortIconClick} ></DataTableColumnHeader>
-            <DataTableColumnHeader fixed top="0" name="name" sortIconTitle="sort by name" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('name')}>Name</DataTableColumnHeader>
-            <DataTableColumnHeader fixed top="0" name="date" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('date')}>Date Created</DataTableColumnHeader>
-            <DataTableColumnHeader fixed top="0" name="status" sortIconTitle="sort by status" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('status')}>Status</DataTableColumnHeader>
-            <DataTableColumnHeader fixed top="0">Publish DHIS2</DataTableColumnHeader>
-            <DataTableColumnHeader fixed top="0" width="48px" ></DataTableColumnHeader>
-          </DataTableRow>
-        </TableHead>
-        <TableBody>{jobs && jobs.sort((a, b) => {
-            const strA = a[column]
-            const strB = b[column]
+      <Card>
+        <DataTable>
+          <TableHead>
+            <DataTableRow>
+              <DataTableColumnHeader fixed top="0" width="48px" onSortIconClick={onSortIconClick} ></DataTableColumnHeader>
+              <DataTableColumnHeader fixed top="0" name="name" sortIconTitle="sort by name" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('name')}>{i18n.t("Name")}</DataTableColumnHeader>
+              <DataTableColumnHeader fixed top="0" name="date" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('date')}>{i18n.t("Date Created")}</DataTableColumnHeader>
+              <DataTableColumnHeader fixed top="0" name="status" sortIconTitle="sort by status" onSortIconClick={onSortIconClick} sortDirection={getSortDirection('status')}>{i18n.t("Status")}</DataTableColumnHeader>
+              <DataTableColumnHeader fixed top="0">{i18n.t("Publish DHIS2")}</DataTableColumnHeader>
+              <DataTableColumnHeader fixed top="0" width="48px"></DataTableColumnHeader>
+            </DataTableRow>
+          </TableHead>
+          <TableBody>{jobs && jobs.sort((a, b) => {
+              const strA = a[column]
+              const strB = b[column]
 
-            if ((direction === 'asc' && strA < strB) ||(direction === 'desc' && strA > strB)) return -1
-            if ((direction === 'desc' && strA < strB) ||(direction === 'asc' && strA > strB)) return 1
-            return 0
-        }).map((job) => {
-          return <JobDetails toggle={toggle} key={job.id} name={job.name} status={job.status} id={job.id} date={job.date}/>
-        })}</TableBody>
-      </DataTable>
-    </Card>
+              if ((direction === 'asc' && strA < strB) ||(direction === 'desc' && strA > strB)) return -1
+              if ((direction === 'desc' && strA < strB) ||(direction === 'asc' && strA > strB)) return 1
+              return 0
+          }).map((job) => {
+            return <JobDetails toggle={toggle} key={job.id} name={job.name} status={job.status} id={job.id} date={job.date}/>
+          })}</TableBody>
+        </DataTable>
+      </Card>
     </div>
   );
 }
