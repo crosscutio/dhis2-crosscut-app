b8bb756eea9b31f93d2e6ad760e8313a0635fcac
Tran Luong <luongt.13@gmail.com>
2022-06-14 18:18:58 -0400

    test

--- src/App.js
+++ src/App.js
@@ -13,159 +13,157 @@ import { getAmplifyPoolData } from './env'
 const poolData = getAmplifyPoolData()
 
 Amplify.configure({
-    Auth: {
-        region: 'us-east-1',
-        ...poolData,
-    },
+  Auth: {
+    region: 'us-east-1',
+    ...poolData,
+  },
 })
 
 // TODO: figure out how to integrate DHIS2 i18n with amplify
 I18n.putVocabulariesForLanguage('en', {
-    'Sign In': 'Crosscut Login', // Tab header
-    'Create Account': 'Create Account', // Tab header
-    'Forgot your password?': 'Reset Password',
+  'Sign In': 'Crosscut Login', // Tab header
+  'Create Account': 'Create Account', // Tab header
+  'Forgot your password?': 'Reset Password',
 })
 
 const formFields = {
-    signIn: {
-        username: {
-            labelHidden: true,
-            placeholder: i18n.t('Email'),
-            isRequired: true,
-        },
-        password: {
-            labelHidden: true,
-            placeholder: i18n.t('Enter your password'),
-        },
+  signIn: {
+    username: {
+      labelHidden: true,
+      placeholder: i18n.t('Email'),
+      isRequired: true,
     },
-    signUp: {
-        email: {
-            labelHidden: true,
-            placeholder: i18n.t('Email'),
-            isRequired: true,
-        },
-        password: {
-            labelHidden: true,
-            placeholder: i18n.t('Enter your password'),
-            isRequired: true,
-        },
-        name: {
-            labelHidden: true,
-            placeholder: i18n.t('Name'),
-            isRequired: true,
-        },
+    password: {
+      labelHidden: true,
+      placeholder: i18n.t('Enter your password'),
     },
+  },
+  signUp: {
+    email: {
+      labelHidden: true,
+      placeholder: i18n.t('Email'),
+      isRequired: true,
+    },
+    password: {
+      labelHidden: true,
+      placeholder: i18n.t('Enter your password'),
+      isRequired: true,
+    },
+    name: {
+      labelHidden: true,
+      placeholder: i18n.t('Name'),
+      isRequired: true,
+    },
+  },
 }
 
 const buildFields = (fields) => {
-    return fields.map((field) => {
-        return i18n.t(field)
-    })
+  return fields.map((field) => {
+    return i18n.t(field)
+  })
 }
 
 // theme added to match the blue for text/tabs/buttons on the login page
 const theme = {
-    name: 'dhis2-theme',
-    tokens: {
-        components: {
-            button: {
-                link: {
-                    color: {
-                        value: '#0d47a1',
-                    },
-                },
-                primary: {
-                    background: {
-                        color: {
-                            value: '#0d47a1',
-                        },
-                    },
-                },
-                _hover: {
-                    color: {
-                        value: '#0d47a1',
-                    },
-                },
-            },
-            tabs: {
-                item: {
-                    _hover: {
-                        color: {
-                            value: '#0d47a1',
-                        },
-                    },
-                    _active: {
-                        color: {
-                            value: '#0d47a1',
-                        },
-                        border: {
-                            color: {
-                                value: '#0d47a1',
-                            },
-                        },
-                    },
-                    _focus: {
-                        color: {
-                            value: '#0d47a1',
-                        },
-                    },
-                },
-            },
+  name: 'dhis2-theme',
+  tokens: {
+    components: {
+      button: {
+        link: {
+          color: {
+            value: '#0d47a1',
+          },
         },
+        primary: {
+          background: {
+            color: {
+              value: '#0d47a1',
+            },
+          },
+        },
+        _hover: {
+          color: {
+            value: '#0d47a1',
+          },
+        },
+      },
+      tabs: {
+        item: {
+          _hover: {
+            color: {
+              value: '#0d47a1',
+            },
+          },
+          _active: {
+            color: {
+              value: '#0d47a1',
+            },
+            border: {
+              color: {
+                value: '#0d47a1',
+              },
+            },
+          },
+          _focus: {
+            color: {
+              value: '#0d47a1',
+            },
+          },
+        },
+      },
     },
+  },
 }
 
 const MyApp = () => {
-    const config = useConfig()
-    setupDHIS2Api(config)
+  const config = useConfig()
+  setupDHIS2Api(config)
 
-    const components = {
-        Footer() {
-            return (
-                <Card>
-                    <div className={classes.instructions}>
-                        <p className={classes.instructionText}>
-                            {i18n.t(
-                                "To use the Microplanning app, you need to log in to your Crosscut account. Don't have an account? You can create one for free by clicking the 'Create Account' tab above or visit "
-                            )}
-                            <a
-                                style={{ color: '#0d47a1' }}
-                                href="https://app.crosscut.io/"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                app.crosscut.io
-                            </a>
-                            .
-                        </p>
-                    </div>
-                </Card>
-            )
-        },
-    }
+  const components = {
+    Footer() {
+      return (
+        <Card>
+          <div className={classes.instructions}>
+            <p className={classes.instructionText}>
+              {i18n.t(
+                "To use the Microplanning app, you need to log in to your Crosscut account. Don't have an account? You can create one for free by clicking the 'Create Account' tab above or visit "
+              )}
+              <a
+                style={{ color: '#0d47a1' }}
+                href="https://app.crosscut.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                app.crosscut.io
+              </a>
+              .
+            </p>
+          </div>
+        </Card>
+      )
+    },
+  }
 
-    return (
-        <AmplifyProvider theme={theme}>
-            <Authenticator
-                formFields={formFields}
-                className={classes.amplify}
-                components={components}
-                signUpAttributes={buildFields(['email', 'password', 'name'])}
-                loginMechanisms={buildFields(['email'])}
-            >
-                {(user) => (
-                    <div className={classes.container}>
-                        <Layout
-                            token={
-                                user?.user?.signInUserSession?.idToken?.jwtToken
-                            }
-                            user={user?.user?.attributes?.email}
-                        />
-                    </div>
-                )}
-            </Authenticator>
-        </AmplifyProvider>
-    )
+  return (
+    <AmplifyProvider theme={theme}>
+      <Authenticator
+        formFields={formFields}
+        className={classes.amplify}
+        components={components}
+        signUpAttributes={buildFields(['email', 'password', 'name'])}
+        loginMechanisms={buildFields(['email'])}
+      >
+        {(user) => (
+          <div className={classes.container}>
+            <Layout
+              token={user?.user?.signInUserSession?.idToken?.jwtToken}
+              user={user?.user?.attributes?.email}
+            />
+          </div>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
+  )
 }
 
 export default MyApp
--- src/README.md
+++ src/README.md
@@ -4,11 +4,11 @@ A DHIS2 application to create catchment areas for health facilities. The DHIS2 C
 
 The app lets you:
 
--   Create catchment areas boundaries for your health facilities and publish them to DHIS2
--   Create visualizations and maps in DHIS2 by combining the catchment areas with DHIS2 reporting data
--   Generate target population estimates for each catchment area to quickly calculate coverage estimates
--   See travel time to the health facilities within the catchment area to support outreach and session planning
--   Requirements: An account with Crosscut (a third party application) and internet access
+- Create catchment areas boundaries for your health facilities and publish them to DHIS2
+- Create visualizations and maps in DHIS2 by combining the catchment areas with DHIS2 reporting data
+- Generate target population estimates for each catchment area to quickly calculate coverage estimates
+- See travel time to the health facilities within the catchment area to support outreach and session planning
+- Requirements: An account with Crosscut (a third party application) and internet access
 
 Submit requests and issues here: [GitHub Issues](https://github.com/crosscutio/dhis2-crosscut-app/issues/new)
 
@@ -16,38 +16,38 @@ Submit requests and issues here: [GitHub Issues](https://github.com/crosscutio/d
 
 ### App setup
 
--   Install the Microplanning app.
--   Set up a free account with Crosscut within the Microplanning app. It takes less than a minute and allows your Microplanning app to access the Crosscut analytics engine. It also allows you to manually edit catchment areas, visualize health site accessibility heat maps, and access other features at [app.crosscut.io](https://app.crosscut.io/).
--   Internet access (specifically to [api-production.app.crosscut.io](https://api-production.app.crosscut.io)) is required to use the Microplanning app.
+- Install the Microplanning app.
+- Set up a free account with Crosscut within the Microplanning app. It takes less than a minute and allows your Microplanning app to access the Crosscut analytics engine. It also allows you to manually edit catchment areas, visualize health site accessibility heat maps, and access other features at [app.crosscut.io](https://app.crosscut.io/).
+- Internet access (specifically to [api-production.app.crosscut.io](https://api-production.app.crosscut.io)) is required to use the Microplanning app.
 
 ### How to create new catchment areas
 
--   Click `Create catchment areas` button.
--   Select the country you want to target.
--   Name the catchment areas.
--   Choose the admin level where your facilities are located.
--   Choose which groups of facilities to include.
--   You can use your catchment areas to create thematic maps and other views in the maps module of DHIS2.
+- Click `Create catchment areas` button.
+- Select the country you want to target.
+- Name the catchment areas.
+- Choose the admin level where your facilities are located.
+- Choose which groups of facilities to include.
+- You can use your catchment areas to create thematic maps and other views in the maps module of DHIS2.
 
 ### Understanding the statuses
 
--   `Pending`: your catchment areas are being created.
--   `Ready`: your catchment areas have been created and ready to be published to DHIS2.
--   `Publishing`: your catchment areas is in the process of being published.
--   `Published`: your catchment areas have been published to DHIS2.
+- `Pending`: your catchment areas are being created.
+- `Ready`: your catchment areas have been created and ready to be published to DHIS2.
+- `Publishing`: your catchment areas is in the process of being published.
+- `Published`: your catchment areas have been published to DHIS2.
 
 ### How to delete catchment areas
 
--   Click delete icon to delete the catchment area.
--   If you have published the catchment areas to DHIS2 then it will be removed from DHIS2.
+- Click delete icon to delete the catchment area.
+- If you have published the catchment areas to DHIS2 then it will be removed from DHIS2.
 
 ### Publish your catchment areas to DHIS2
 
--   Click `Publish` button to connect your catchments to DHIS2.
--   Once published, you can access the catchment areas in DHIS2
--   Click `Unpublish` button to remove access to the catchment areas in DHIS2.
+- Click `Publish` button to connect your catchments to DHIS2.
+- Once published, you can access the catchment areas in DHIS2
+- Click `Unpublish` button to remove access to the catchment areas in DHIS2.
 
 ### Catchment areas details
 
--   Click details icon to see details on the catchment areas.
--   Details will only show for catchment areas created in the DHIS2 app.
+- Click details icon to see details on the catchment areas.
+- Details will only show for catchment areas created in the DHIS2 app.
