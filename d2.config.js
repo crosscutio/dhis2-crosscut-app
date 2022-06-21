6c9f730d62b4e4528d126909e2e333a901280fec
Tran Luong <luongt.13@gmail.com>
2022-06-21 11:41:27 -0400

    ignore dhis2 config

--- d2.config.js
+++ d2.config.js
@@ -1,11 +1,11 @@
 const config = {
-  id: "de19ff76-3459-4ec1-a881-5b8644cd6c51",
-  type: "app",
-  name: "Microplanning",
+  id: 'de19ff76-3459-4ec1-a881-5b8644cd6c51',
+  type: 'app',
+  name: 'Microplanning',
   entryPoints: {
-    app: "./src/App.js",
+    app: './src/App.js',
   },
-  minDHIS2Version: "2.38",
+  minDHIS2Version: '2.38',
 };
 
 module.exports = config;
